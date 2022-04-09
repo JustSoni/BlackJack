c988fb61a9d7eeb9b5f61b201b65421031d1aa1b
JustSoni <78814531+JustSoni@users.noreply.github.com>
2022-04-10 00:31:23 +0300

    Added v1.0
    Ace is only count as 11! Has to be fixed to be 1 or 11.

---
+++ BlackJack/BlackJack/Card.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+    public class Card
+    {
+        public int Value;
+        public static string[] SuitsArray = new string[] { "Hearts", "Diamonds", "Clubs", "Spades" };
+        public string Suit;
+
+        public Card(int value, string suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+
+        public int Power
+        {
+            get
+            {
+                if (Value < 10)
+                    return Value;
+                else if(Value<14)
+                {
+                    return 10;
+                }
+                else
+                {
+                    return 11;
+                }
+            }
+        }
+
+        public Card(string input)
+        {
+            string tempValue = "";
+            string suitSentence = "";
+            switch (Value)
+            {
+                case 11:
+                    tempValue = "Jack";
+                    break;
+                case 12:
+                    tempValue = "Queen";
+                    break;
+                case 13:
+                    tempValue = "King";
+                    break;
+                case 14:
+                    tempValue = "Ace";
+                    break;
+                default:
+                    tempValue = Value.ToString();
+                    break;
+            }
+            switch (Suit)
+            {
+                case "Hearts":
+                    suitSentence = " of Hearts";
+                    break;
+                case "Diamonds":
+                    suitSentence = " of Diamonds";
+                    break;
+                case "Clubs":
+                    suitSentence = " of Clubs";
+                    break;
+                case "Spades":
+                    suitSentence = " of Spades";
+                    break;
+            }
+            input = tempValue + suitSentence;
+        }
+
+        public override string ToString()
+        {
+            string tempValue = "";
+            string suitSentence = "";
+            switch (this.Value)
+            {
+                case 11:
+                    tempValue = "Jack";
+                    break;
+                case 12:
+                    tempValue = "Queen";
+                    break;
+                case 13:
+                    tempValue = "King";
+                    break;
+                case 14:
+                    tempValue = "Ace";
+                    break;
+                default:
+                    tempValue = this.Value.ToString();
+                    break;
+            }
+            switch (this.Suit)
+            {
+                case "Hearts":
+                    suitSentence = " of Hearts";
+                    break;
+                case "Diamonds":
+                    suitSentence = " of Diamonds";
+                    break;
+                case "Clubs":
+                    suitSentence = " of Clubs";
+                    break;
+                case "Spades":
+                    suitSentence = " of Spades";
+                    break;
+            }
+            return $"{tempValue} of {suitSentence}";
+        }
+    }
+}
---
+++ BlackJack/BlackJack/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace BlackJack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deck.FillDeck();
+            //Deck.PrintDeck();
+
+            Deck deck = new Deck();
+
+            deck.ShuffleDeck();
+
+            //Console.WriteLine(deck.Count);
+
+            int chips = 0;
+            int bet = 0;
+
+            Console.WriteLine("With how much chips would you like to start?");
+            Console.Write("Chip -> ");
+            chips = int.Parse(Console.ReadLine());
+
+            string move = "";
+
+
+            while (true)
+            {
+                Console.WriteLine($"Remaining chips = {chips}");
+                List<Card> playerHand = new List<Card>();
+                List<Card> dealerHand = new List<Card>();
+
+                Messages.DisplayOptions();
+                bet = int.Parse(Console.ReadLine());
+
+                while (bet > chips)
+                {
+                    Messages.InsufficientFundsRebet();
+                    bet = int.Parse(Console.ReadLine());
+                }
+
+                chips -= bet;
+
+                Messages.Dealing();
+                playerHand.Add(deck.DealStackCard());
+                dealerHand.Add(deck.DealStackCard());
+                playerHand.Add(deck.DealStackCard());
+
+                Messages.ShowCards(playerHand, true); // True -> Player
+                Messages.ShowCards(dealerHand, false); // False -> Dealer
+
+                while (playerHand.Select(x => x.Power).Sum() < 21) // Player's turn
+                {
+                    Messages.NextMove();
+                    move = Console.ReadLine();
+                    while (!(move == "Hit" || move == "Stand" || move == "Double"))
+                    {
+                        Console.WriteLine("Wrong option, try again -> ");
+                        move = Console.ReadLine();
+                    }
+
+                    if (move == "Hit")
+                    {
+                        Messages.Dealing();
+                        playerHand.Add(deck.DealStackCard());
+                    }
+                    else if (move == "Stand")
+                    {
+                        break;
+                    }
+                    //else(move=="Double")
+                    //{
+                    //    bet += bet;
+                    //    Messages.Dealing();
+                    //    playerHand.Add(deck.DealStackCard());
+                    //}
+
+                    Messages.ShowCards(playerHand, true); // True -> Player
+                    Messages.ShowCards(dealerHand, false); // False -> Dealer
+                }
+
+                if (playerHand.Select(x => x.Power).Sum() > 21)
+                {
+                    Messages.Busted();
+                }
+                else if (playerHand.Select(x => x.Power).Sum() == 21 && playerHand.Count == 2)
+                {
+                    Messages.Dealing();
+                    Thread.Sleep(1000);
+                    dealerHand.Add(deck.DealStackCard());
+                    Thread.Sleep(1000);
+
+                    Messages.ShowCards(playerHand, true); // True -> Player
+                    Messages.ShowCards(dealerHand, false); // False -> Dealer
+
+                    Thread.Sleep(1000);
+
+                    if (dealerHand.Select(x => x.Power).Sum() < 21)
+                    {
+                        Messages.WinBlackjack(bet);
+                        chips += (int)(bet * 2.5);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Dealer has also a blackjack! PUSH");
+                        Console.WriteLine("Money returned!");
+                        Console.ResetColor();
+                        chips += bet;
+                    }
+                }
+                else
+                {
+                    Messages.Dealing();
+                    Thread.Sleep(1000);
+                    dealerHand.Add(deck.DealStackCard());
+                    Thread.Sleep(1000);
+                    while (dealerHand.Select(x => x.Power).Sum() < 17)
+                    {
+                        dealerHand.Add(deck.DealStackCard());
+                        Thread.Sleep(1000);
+                    }
+
+                    if (dealerHand.Select(x => x.Power).Sum() < playerHand.Select(x => x.Power).Sum() || dealerHand.Select(x => x.Power).Sum()>21)
+                    {
+                        Messages.ShowCards(playerHand, true); // True -> Player
+                        Messages.ShowCards(dealerHand, false); // False -> Dealer
+
+                        //Console.ForegroundColor = ConsoleColor.Green;
+                        //Console.WriteLine($"You win! Received {bet} chips");
+
+
+                        Messages.Win(bet);
+
+                        //Console.ResetColor();
+                        chips += bet * 2;
+                    }
+                    else if (dealerHand.Select(x => x.Power).Sum() == playerHand.Select(x => x.Power).Sum())
+                    {
+                        Messages.ShowCards(playerHand, true); // True -> Player
+                        Messages.ShowCards(dealerHand, false); // False -> Dealer
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"It is a push! {bet} chips returned;");
+                        Console.ResetColor();
+                        chips += bet;
+                    }
+                    else
+                    {
+                        Messages.ShowCards(playerHand, true); // True -> Player
+                        Messages.ShowCards(dealerHand, false); // False -> Dealer
+
+                        Messages.DealerWin();
+
+                        Console.WriteLine();
+                    }
+
+
+
+                }
+
+
+            }
+        }
+    }
+}
